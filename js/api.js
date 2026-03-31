6f2762756d6d70ea3a2e7f331ba3d28cf3b97414
abdo74174 <g1g2g3g4g5m1m2m3m4m5@gmail.com>
2026-03-31 07:39:31 +0200

    feat: implement API service module with authentication and fetch utility functions

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
     URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
          ? 'http://localhost:5239/api' 
-         : '/api'
+         : 'https://noor-muzdalifah.runasp.net/api'
 };
 
 
